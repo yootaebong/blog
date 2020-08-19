18a90e2d0c5c27a15866b40f34c8bc6dc8d7975f
youtaebong <ytb4748@gmail.com>
2020-08-19 23:31:29 +0900

    디자인 패턴 정리

---
+++ Controllers/index.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const initData = () => {};
+
+const lifeCycle = async () => {
+  await initData();
+};
+
+lifeCycle();
+
+//# sourceMappingURL=index.js.map
--- ControllersJS/index.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-var lifeCycle = function () {
-    console.log('index.js');
-};
-export { lifeCycle };
-//# sourceMappingURL=index.js.map
